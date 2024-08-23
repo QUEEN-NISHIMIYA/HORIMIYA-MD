5d52fee7343cbd3dfe9c14f56235d3443c48aedc
ɪɀꪊᴍιꫀ ᥊ꫀꪀρꪖι <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-08-23 22:32:21 +0530

    Update Horimiya.js

--- Horimiya.js
+++ Horimiya.js
@@ -375,7 +375,7 @@ async function connectionUpdate(update) {
   if (connection === 'open') {
     const { jid, name } = conn.user
 
-    let msgf = `Hai🤩${name} Congrats you have successfully deployed HORIMIYA-BOT\nJoin my support Group for any Query\n https://chat.whatsapp.com/Krn1VGvyWqP5brTWbNoZsp`
+    let msgf = `Hai🤩${name} Congrats you have successfully deployed GURU-BOT\nJoin my support Group for any Query\n https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp`
 
     let gmes = conn.sendMessage(
       jid,
