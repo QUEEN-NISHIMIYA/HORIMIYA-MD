e82810972f030452c08965437077558805f4fced
ɪɀꪊᴍιꫀ ᥊ꫀꪀρꪖι <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-08-26 19:13:39 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- plugins/menu.js
+++ plugins/menu.js
@@ -11,6 +11,402 @@ const time = moment.tz('Asia/Kolkata').format('HH')
 let wib = moment.tz('Asia/Kolkata').format('HH:mm:ss')
 //import db from '../lib/database.js'
 
+
+let groupmenu = `
+   ✦ ───『 *group* 』─── ⚝
+  ◈ .getbio <@tag/reply>  Ⓛ
+  ◈ .Setdesc <text>
+  ◈ .setname <text>
+  ◈ .add
+  ◈ .delete
+  ◈ .delwarn @user
+  ◈ .demote (@tag)
+  ◈ .infogp
+  ◈ .hidetag
+  ◈ .invite <917xxx>
+  ◈ .kick @user
+  ◈ .link
+  ◈ .poll question|option|option
+  ◈ .profile
+  ◈ .promote
+  ◈ .resetlink
+  ◈ .setbye <text>
+  ◈ .group *open/close*
+  ◈ .setwelcome <text>
+  ◈ .simulate <event> @user
+  ◈ .staff
+  ◈ .tagall
+  ◈ .totag
+  ◈ .warn @user
+  ◈ .warns
+  ◈ .main
+  ╰──────────⳹`
+
+let ownermenu = `
+  ✦ ───『 *owner* 』─── ⚝
+  ◈ .addprem <@tag>
+  ◈ .addowner @user
+  ◈ .allow <@tag>
+  ◈ .HEROKU
+  ◈ .ban @user
+  ◈ .banchat
+  ◈ .tx
+  ◈ .broadcastgroup <text>
+  ◈ .bcgc <text>
+  ◈ .cleartmp
+  ◈ .delexpired
+  ◈ .delprem @user
+  ◈ .removeowner @user
+  ◈ .setppbotfull
+  ◈ .getplugin <name file>
+  ◈ .getfile <name file>
+  ◈ .join <chat.whatsapp.com> <dias>
+  ◈ .reset <54xxx>
+  ◈ .resetprefix
+  ◈ .restart
+  ◈ .setprefix
+  ◈ .setprefix [symbol]
+  ◈ .unban @user
+  ◈ .unbanchat
+  ◈ .update
+  ◈ .config
+  ◈ .listban
+  ◈ .deleteplugin <name>
+  ╰──────────⳹`
+
+let funmenu = `
+  ✦ ───『 *fun* 』─── ⚝
+  ◈ .afk <reason>
+  ◈ .tomp3
+  ◈ .toav
+  ◈ .bot
+  ◈ .character @tag
+  ◈ .dare
+  ◈ .flirt
+  ◈ .gay @user
+  ◈ .image
+  ╰──────────⳹`
+
+let reactmenu = `
+  ✦ ───『 *reaction* 』─── ⚝
+  ◈ .bully @tag
+  ◈ .cuddle @tag
+  ◈ .cry @tag
+  ◈ .hug @tag
+  ◈ .awoo @tag
+  ◈ .kiss @tag
+  ◈ .lick @tag
+  ◈ .pat @tag
+  ◈ .smug @tag
+  ◈ .bonk @tag
+  ◈ .yeet @tag
+  ◈ .blush @tag
+  ◈ .smile @tag
+  ◈ .wave @tag
+  ◈ .highfive @tag
+  ◈ .handhold @tag
+  ◈ .nom @tag
+  ◈ .bite @tag
+  ◈ .glomp @tag
+  ◈ .slap @tag
+  ◈ .kill @tag
+  ◈ .happy @tag
+  ◈ .wink @tag
+  ◈ .poke @tag
+  ◈ .dance @tag
+  ◈ .cringe @tag
+  ╰──────────⳹`
+
+let dlmenu = `
+  ✦ ───『 *downloader* 』─── ⚝
+  ◈ .facebook <url>
+  ◈ .gdrive 🅟
+  ◈ .gitclone <url>
+  ◈ .igstalk
+  ◈ .instagram
+  ◈ .mediafire <url>
+  ◈ .mega
+  ◈ .modapk
+  ◈ .play <query>
+  ◈ .play2 <text>
+  ◈ .playvid <text>
+  ◈ .spotify
+  ◈ .tiktok <url>
+  ◈ .tiktokstalk
+  ◈ .twitter <url>
+  ◈ .ytmp3 <url>
+  ◈ .ytsearch
+  ◈ .ytmp4 <yt-link>
+  ◈ .wallpaper <query>
+  ╰──────────⳹`
+
+let logomenu = `
+  ✦ ───『 *maker* 』─── ⚝
+  ◈ .blur
+  ◈ .difuminar2
+  ◈ .hornycard
+  ◈ .hornylicense
+  ◈ .gfx1
+  ◈ .gfx2
+  ◈ .gfx3
+  ◈ .gfx4
+  ◈ .gfx5
+  ◈ .gfx6
+  ◈ .gfx7
+  ◈ .gfx8
+  ◈ .gfx9
+  ◈ .gfx10
+  ◈ .gfx11
+  ◈ .gfx12
+  ◈ .simpcard
+  ◈ .itssostupid
+  ◈ .iss
+  ◈ .stupid
+  ◈ .tweet <comment>
+  ◈ .lolicon
+  ◈ .ytcomment <comment>
+  ╰──────────⳹`
+
+let stickermenu = `
+  ✦ ───『 *sticker* 』─── ⚝
+  ◈ .emojimix <emoji+emoji>
+  ◈ .getsticker
+  ◈ .smaker
+  ◈ .stickerwithmeme (caption|reply media)
+  ◈ .swmeme <url>
+  ◈ .swm(caption|reply media)
+  ◈ .sfull
+  ◈ .toimg <sticker>
+  ◈ .tovid
+  ◈ .trigger <@user>
+  ◈ .ttp
+  ◈ .ttp2
+  ◈ .ttp3
+  ◈ .ttp4
+  ◈ .ttp5
+  ◈ .attp
+  ◈ .attp2
+  ◈ .attp3
+  ◈ .take <name>|<author>
+  ╰──────────⳹`
+
+let audiomenu = `
+  ✦ ───『 *audio* 』─── ⚝
+  ◈ .bass [vn]
+  ◈ .blown [vn]
+  ◈ .deep [vn]
+  ◈ .earrape [vn]
+  ◈ .fast [vn]
+  ◈ .fat [vn]
+  ◈ .nightcore [vn]
+  ◈ .reverse [vn]
+  ◈ .robot [vn]
+  ◈ .slow [vn]
+  ◈ .smooth [vn]
+  ◈ .tupai [vn]
+  ╰──────────⳹`
+
+let xenpaimenu = `
+  ✦ ───『 *xenpai* 』─── ⚝
+  ◈ .unban number
+  ◈ .crypto news
+  ◈ .panels
+  ◈ .script
+  ╰──────────⳹
+  `
+
+let animemenu = `
+  ✦ ───『 *anime* 』─── ⚝
+  ◈ .anime
+  ◈ .akira
+  ◈ .akiyama
+  ◈ .anna
+  ◈ .asuna
+  ◈ .ayuzawa
+  ◈ .boruto
+  ◈ .chiho
+  ◈ .chitoge
+  ◈ .deidara
+  ◈ .erza
+  ◈ .elaina
+  ◈ .eba
+  ◈ .emilia
+  ◈ .hestia
+  ◈ .hinata
+  ◈ .inori
+  ◈ .isuzu
+  ◈ .itachi
+  ◈ .itori
+  ◈ .kaga
+  ◈ .kagura
+  ◈ .kaori
+  ◈ .keneki
+  ◈ .kotori
+  ◈ .kurumi
+  ◈ .madara
+  ◈ .mikasa
+  ◈ .miku
+  ◈ .minato
+  ◈ .naruto
+  ◈ .nezuko
+  ◈ .sagiri
+  ◈ .sasuke
+  ◈ .sakura
+  ◈ .manhwa
+  ◈ .waifu
+  ◈ .neko
+  ◈ .zerotwo
+  ◈ .loli
+  ◈ .pokedex <pokemon>
+  ◈ .trace
+  ╰──────────⳹
+  `
+let nsfwmenu = `
+  ✦ ───『 *nsfw* 』─── ⚝
+  ◈ .genshin
+  ◈ .swimsuit
+  ◈ .schoolswimsuit
+  ◈ .white
+  ◈ .barefoot
+  ◈ .touhou
+  ◈ .gamecg
+  ◈ .hololive
+  ◈ .uncensored
+  ◈ .sunglasses
+  ◈ .glasses
+  ◈ .weapon
+  ◈ .shirtlift
+  ◈ .chain
+  ◈ .fingering
+  ◈ .flatchest
+  ◈ .torncloth
+  ◈ .bondage
+  ◈ .demon
+  ◈ .wet
+  ◈ .pantypull
+  ◈ .headdress
+  ◈ .headphone
+  ◈ .tie
+  ◈ .anusview
+  ◈ .shorts
+  ◈ .stokings
+  ◈ .topless
+  ◈ .beach
+  ◈ .bunnygirl
+  ◈ .bunnyear
+  ◈ .idol
+  ◈ .vampire
+  ◈ .gun
+  ◈ .maid
+  ◈ .bra
+  ◈ .nobra
+  ◈ .bikini
+  ◈ .whitehair
+  ◈ .blonde
+  ◈ .pinkhair
+  ◈ .bed
+  ◈ .ponytail
+  ◈ .nude
+  ◈ .dress
+  ◈ .underwear
+  ◈ .foxgirl
+  ◈ .uniform
+  ◈ .skirt
+  ◈ .sex
+  ◈ .sex2
+  ◈ .sex3
+  ◈ .breast
+  ◈ .twintail
+  ◈ .spreadpussy
+  ◈ .tears
+  ◈ .seethrough
+  ◈ .breasthold
+  ◈ .drunk
+  ◈ .fateseries
+  ◈ .spreadlegs
+  ◈ .openshirt
+  ◈ .headband
+  ◈ .food
+  ◈ .close
+  ◈ .tree
+  ◈ .nipples
+  ◈ .erectnipples
+  ◈ .horns
+  ◈ .greenhair
+  ◈ .wolfgirl
+  ◈ .catgirl
+  ◈ .nsfw
+  ◈ .ass
+  ◈ .boobs
+  ◈ .lesbian
+  ◈ .pussy
+  ◈ .pack
+  ◈ .xvid
+  ◈ .xnxx
+  ╰──────────⳹`
+
+let toolsmenu = `
+  ✦ ───『 *tools* 』─── ⚝
+  ◈ .nowa
+  ◈ .qr <text>
+  ◈ .qrcode <text>
+  ◈ .style <key> <text>
+  ◈ .recolor
+  ◈ .hdr
+  ◈ .length <amount>
+  ◈ .tinyurl <link>
+  ◈ .shorten <link>
+  ◈ .shazam
+  ◈ .define <word>
+  ◈ .itunes
+  ◈ .lyrics
+  ◈ .imdb
+  ◈ .randomcourse
+  ◈ .readvo
+  ◈ .removebg
+  ◈ .tourl
+  ◈ .translate <lang> <text>
+  ◈ .true
+  ◈ .tts <lang> <task>
+  ╰──────────⳹`
+
+let aimenu = `
+  ✦ ───『 *AI* 』─── ⚝
+  ◈ .toanime
+  ◈ .tocartoon
+  ◈ .ai
+  ◈ .bard
+  ◈ .alexa
+  ◈ .bingimg
+  ╰──────────⳹
+  `
+
+let botmenu = `
+  ✦ ───『 *Bot Menu* 』─── ⚝
+  ◈ .ping
+  ◈ .runtime
+  ◈ .server
+  ◈ .blocklist
+  ◈ .alive
+  ◈ .info
+  ◈ .owner
+  ◈ .totalfeature
+  ◈ .list
+  ◈ .ppcouple
+  ◈ .ppcp
+  ◈ .pinterest
+  ◈ .reg <name.age>
+  ◈ .mysn
+  ◈ .unreg 
+  ╰──────────⳹
+  `
+let pluginmenu = `
+  ✦ ───『 *plugin* 』─── ⚝
+  ◈ .plugins
+  ◈ .install <Gist URL>
+  ╰──────────⳹
+  `
+
 let handler = async (m, { conn, usedPrefix, command }) => {
   let d = new Date(new Date() + 3600000)
   let locale = 'en'
@@ -64,12 +460,17 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
 🎀Type the menu name with prefix to get desired menu
 1.ownermenu
-2.platinummenu
+2.logomenu
 3.aimenu
 4.stickermenu
-5.downloadmenu
+5.dlmenu
 6.audiomenu
-
+7.toolsmenu
+8.botmenu
+9.nsfwmenu
+10.xenpaimenu
+11.animemenu
+12.reactmenu
 `
 
   conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, null, rpyt)
