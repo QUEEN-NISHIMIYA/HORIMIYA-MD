043475456b395b9279e0d851e61800c19003e32c
ɪɀꪊᴍιꫀ ᥊ꫀꪀρꪖι <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-10-11 08:52:11 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- plugins/bot.js
+++ plugins/bot.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
   let av = `./Assets/mp3/${pickRandom(['Jeene', 'Karimizhi', 'Onnam', 'Sundariye', 'Vennila'])}.mp3`
 
-  m.reply(`Hello ${taguser} Need help?  type /help `)
+  m.reply(`Hello ${taguser} Need help?  type .help `)
   conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
 
