a0ad50ce28f8d18dedb433b2fef4a8c2d660c032
ɪɀꪊᴍιꫀ ᥊ꫀꪀρꪖι <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-10-11 08:51:23 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- plugins/bot.js
+++ plugins/bot.js
@@ -2,13 +2,13 @@ let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender]
   let name = conn.getName(m.sender)
   let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
-  let av = `./Assets/mp3/${pickRandom(['Guru', 'Guru1', 'Guru2', 'Guru3', 'Guru4'])}.mp3`
+  let av = `./Assets/mp3/${pickRandom(['Jeene', 'Karimizhi', 'Onnam', 'Sundariye', 'Vennila'])}.mp3`
 
   m.reply(`Hello ${taguser} Need help?  type /help `)
   conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(bot|izumie)$/i
 handler.command = new RegExp()
 
 export default handler
