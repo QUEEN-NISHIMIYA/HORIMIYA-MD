fbbeec1621d7717b5ef779dcb433f11211143721
ɪɀꪊᴍιꫀ ᥊ꫀꪀρꪖι <103162659+QUEEN-NISHIMIYA@users.noreply.github.com>
2024-10-13 21:26:52 +0530

    💗ɪᴢᴜᴍɪᴇ-ᴋᴜɴ💗

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
   m.reply(
     `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
       users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n└──✪ GURU ┃ ᴮᴼᵀ ✪──',
+      '\n└──✪ HORI ┃ ᴮᴼᵀ ✪──',
     null,
     {
       mentions: users,
@@ -14,7 +14,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
 handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = ['tagall']
-handler.admin = true
 handler.group = true
+handler.premium = true
 
 export default handler
